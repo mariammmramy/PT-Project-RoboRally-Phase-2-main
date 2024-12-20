906d450de46bb1fc357915d75a4dc2fc4ed3a292
Ziadkh06 <ziadkhaledsaif@gmail.com>
2024-12-20 21:58:40 +0200

    correcting the validation

--- AddWaterpitAction.cpp
+++ AddWaterpitAction.cpp
@@ -29,7 +29,11 @@ void AddWaterpitAction::ReadActionParameters()
 	
 	// 4- Make the needed validations on the read parameters
 
+<<<<<<< Updated upstream
 	if (WaterpitPos.VCell() == WaterpitPos.HCell() == 0)
+=======
+	if (WaterpitPos.VCell() == 4 && WaterpitPos.HCell() == 0)
+>>>>>>> Stashed changes
 	{
 		pOut->PrintMessage("You can't put a game object in the first cell");
 		return;
