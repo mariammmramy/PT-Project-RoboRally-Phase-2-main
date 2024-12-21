058ec3f0124fb3788102b750d4fe2adf13496325
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-21 21:55:30 +0200

    updated to play

--- ToPlayModeAction.cpp
+++ ToPlayModeAction.cpp
@@ -19,38 +19,38 @@ void ToPlayModeAction::ReadActionParameters() {  //(in progress)
 	pOut->ClearStatusBar();
 }
 void ToPlayModeAction::Execute(){    //Switch to play mode (in progress)
-	/*ReadActionParameters();
-	Grid* pGrid = pManager->GetGrid();
-	Output* pOut = pGrid->GetOutput();
+	ReadActionParameters();
+	/*Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput()*/;
 
-	pOut->CreatePlayModeToolBar();*/
+	//pOut->CreatePlayModeToolBar();
 
 }
 void ToPlayModeAction::ExecuteAction(ActionType ActType) {
 
-	Action* pAct = NULL;
+	/*Action* pAct = NULL;*/
 
 	// According to Action Type, create the corresponding action in play mode
 	//in progress
-	switch (ActType)
-	{
-	case(REBOOT):
-		//pAct->
-		break;
-	case(USE_TOOLKIT):
-		break;
-	case(USE_HACK):
-		break;
-	case(NEW):
-		break;
-	case(EXITP):
-		break; 
-	}
+	//switch (ActType)
+	//{
+	//case(REBOOT):
+	//	//pAct->
+	//	break;
+	//case(USE_TOOLKIT):
+	//	break;
+	//case(USE_HACK):
+	//	break;
+	//case(NEW):
+	//	break;
+	//case(EXITP):
+	//	break; 
+	//}
 	// Execute the created action
-	if (pAct != NULL)
-	{
-		pAct->Execute(); // Execute
-		delete pAct;	 // Action is not needed any more after executing ==> delete it
-		pAct = NULL;
-	}
+	//if (pAct != NULL)
+	//{
+	//	pAct->Execute(); // Execute
+	//	delete pAct;	 // Action is not needed any more after executing ==> delete it
+	//	pAct = NULL;
+	//}
 }
