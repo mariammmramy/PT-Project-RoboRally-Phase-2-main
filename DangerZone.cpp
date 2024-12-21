76b8b38ecbbec85bc09206aa39e31f08fa60a3b8
Ziadkh06 <ziadkhaledsaif@gmail.com>
2024-12-22 01:03:27 +0200

    Update DangerZone.cpp

--- DangerZone.cpp
+++ DangerZone.cpp
@@ -1,5 +1,5 @@
 #include "DangerZone.h"
-
+#include "Player.h"
 
 
 DangerZone::DangerZone(const CellPosition & dangerZonePosition): GameObject(dangerZonePosition)
@@ -19,10 +19,19 @@ void DangerZone::Apply(Grid * pGrid, Player * pPlayer)
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1- Print a message "You have reached a danger zone. Click to continue ..." and wait mouse click
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	int x, y;
+
+	pOut->PrintMessage("You have reached a danger zone. Click to continue");
+	pIn->GetPointClicked(x, y);
 
 	// 2- Apply the danger zone's effect by reducing the health of the player by 1 
+	int health = pPlayer->GetHealth();
+	pPlayer->SetHealth(health - 1);
+
 	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
-	
+	pGrid->UpdateInterface();
 }
 
 
