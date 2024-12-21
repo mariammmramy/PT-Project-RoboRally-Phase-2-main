34c5ca0e0391a46d51b370e4b22cd17937fc81d6
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-21 20:41:19 +0200

    changes in play mode

--- ToPlayModeAction.h
+++ ToPlayModeAction.h
@@ -8,7 +8,7 @@ public:
 	ToPlayModeAction(ApplicationManager* pApp);
 	virtual void ReadActionParameters(); 
 	virtual void Execute();  // Executes switch to play mode
-
 	virtual ~ToPlayModeAction();
+	void ExecuteAction(ActionType ActType);//function to excute action types with switch case
 };
 
