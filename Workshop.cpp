29d84295a2e98eb03ceb3d0127362c7204509aa2
Ziadkh06 <ziadkhaledsaif@gmail.com>
2024-12-23 00:44:57 +0200

    Update Workshop.cpp

--- Workshop.cpp
+++ Workshop.cpp
@@ -22,7 +22,9 @@ void Workshop::Apply(Grid * pGrid, Player * pPlayer)
 	pOut->PrintMessage("You have reached a Workshop. Click to continue");
 	pIn->GetPointClicked(x, y);
 
-	pOut->PrintMessage("Type 1 if you want to buy a toolkit, Type 2 if you want to buy a hack device, Type 3 if you want to buy a double laser, Click on the repair icon in the toolbar if you want to repair and reboot");
+	pOut->PrintMessage("Type 1 if you want to buy a toolkit, Type 2 if you want to buy a hack device, click to view the rest of the text");
+	pIn->GetPointClicked(x, y);
+	pOut->PrintMessage("Type 3 if you want to buy a double laser, Press anything and click on the repair icon in the toolbar if you want to repair and reboot");
 
 	int a = pIn->GetInteger(pOut);
 	switch (a)
@@ -30,9 +32,38 @@ void Workshop::Apply(Grid * pGrid, Player * pPlayer)
 	case 1:
 		if (pPlayer->gettoolkit())
 		{
-			pGrid->PrintErrorMessage("You already have a toolkit, you can't buy more than one");
+			pGrid->PrintErrorMessage("You already have a toolkit, you can't buy more than one. Click to continue...");
 		}
+		else
+		{
+			pPlayer->settoolkit(1);
+		}
+		break;
+	case 2:
+		if (pPlayer->gethackdevice())
+		{
+			pGrid->PrintErrorMessage("You already have a hack device, you can't buy more than one. Click to continue...");
+		}
+		else
+		{
+			pPlayer->sethackdevice(1);
+		}
+		break;
+	case 3:
+		if (pPlayer->getweapon())
+		{
+			pGrid->PrintErrorMessage("You already have a double laser. Click to continue...");
+		}
+		else
+		{
+			pPlayer->setweapon(1);
+		}
+		break;
+	default:
+		break;
 	}
+	pIn->GetPointClicked(x, y);
+	pGrid->UpdateInterface();
 }
 
 Workshop::~Workshop()
