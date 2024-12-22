04b5f52e4b0c5961aa4d0fee26f5b15d6b234925
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-22 09:14:58 +0200

    add missing image

--- Output.cpp
+++ Output.cpp
@@ -323,11 +323,11 @@ void Output::CreatePlayModeToolBar() const
 		ITM_USE_HACK,
 		ITM_NEW,
 		ITM_EXITP,*/
+	MenuItemImages[ITM_REBOOT] = "images\\Repair.jpg";
 	MenuItemImages[ITM_USE_TOOLKIT] = "images\\Toolkit.jpg";
 	MenuItemImages[ITM_USE_HACK] = "images\\Hack.jpg";
 	MenuItemImages[ITM_NEW] = "images\\New.jpg";
 	MenuItemImages[ITM_EXITP] = "images\\Menu_Exit.jpg";
-
 
 
 	// Draw menu item one image at a time
