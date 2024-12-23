88fc12d8dde0ca8d3d52705d2511fd1b11f74b87
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-23 22:28:51 +0200

    changed availablecommandnum and savedcommandnum

--- ToPlayModeAction.cpp
+++ ToPlayModeAction.cpp
@@ -27,24 +27,27 @@ void ToPlayMode::Execute(){    //Switch to play mode
 	Grid* pGrid = pManager->GetGrid();
 	Output* pOut = pGrid->GetOutput();
 	Player* pPlayer = pGrid->GetCurrentPlayer();
-	Command commands[5] = { NO_COMMAND };
+	Command commands[10] = { NO_COMMAND };
 	//get random commands
 	
 	srand(time(NULL)); //initialize the random seed
+	
+	int availableCommandNum = pPlayer->GetHealth();
+	int savedCommandNum;
+	if (pPlayer->GetHealth() >= 5) {
+		savedCommandNum = 5;
+	}
+	else {
+		savedCommandNum = pPlayer->GetHealth();
 
-	for (int i = 0; i < 5; i++) {
-		if (pPlayer->GetHealth() >= 8) {
-			int RandIndex = rand() % 8; //generates a random number between 0 and 7
-			commands[i] = availableCommands[RandIndex];
-		}
-		else {
-			int RandIndex = rand() % (pPlayer->GetHealth()); //generates a random number between 0 and 7
-			commands[i] = availableCommands[RandIndex];
-		}
+	}
+	for (int i = 0; i < availableCommandNum; i++) {
+		int RandIndex = rand() % availableCommandNum; //generates a random number between 0 and 7
+		commands[i] = availableCommands[RandIndex];
 	}
 
 	pOut->CreatePlayModeToolBar();
-	pOut->CreateCommandsBar(savedCommands,5,commands, pPlayer->GetHealth()); //commands bar with random commands
+	pOut->CreateCommandsBar(savedCommands, savedCommandNum,commands, availableCommandNum); //commands bar with random commands
 	pOut->PrintMessage("Switched to play mode...");
 	pGrid->UpdateInterface();
 	pGrid->SetEndGame(false);
