db101ed4eaff4329945112bd2f4666c77a6aaeaf
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-23 10:27:54 +0200

    new action class

--- NewAction.h
+++ NewAction.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Action.h"
+#include "Grid.h"
+#include "Player.h"
 class NewAction :public Action
 {
+    NewAction(ApplicationManager* pApp);
+    virtual void ReadActionParameters();
+    virtual void Execute();// Executes new action
+    virtual ~NewAction();
 };
-
