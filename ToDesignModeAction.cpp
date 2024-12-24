5ea97fe964169adffea21d7bef29791ed2349c3a
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2024-12-24 16:37:22 +0200

    Update ToDesignModeAction.cpp

--- ToDesignModeAction.cpp
+++ ToDesignModeAction.cpp
@@ -11,20 +11,32 @@ void ToDesignModeAction::ReadActionParameters() {
 	Grid* pGrid = pManager->GetGrid();
 	Output* pOut = pGrid->GetOutput();
 	Input* pIn = pGrid->GetInput();
-
-
+	pOut->PrintMessage("Game is ended! Switching to Design Mode... Click anywhere to confirm.");
+	int x, y;
+	pIn->GetPointClicked(x, y); // wait for user to click anywhere
 	pOut->ClearStatusBar();
+
 }
 void ToDesignModeAction::Execute() {    //Switch to Design mode 
 	ReadActionParameters();
 	Grid* pGrid = pManager->GetGrid();
 	Output* pOut = pGrid->GetOutput();
 
+	//code to delete game objects from cells 
+   for (int i = 0; i < NumVerticalCells; ++i) {
+       for (int j = 0; j < NumHorizontalCells; ++j) {
+           CellPosition cellPos(i, j);
+           if (pGrid->Getgameobjectfromcell(cellPos)) {
+               pGrid->RemoveObjectFromCell(cellPos); //delete each game object in cell
+           }
+       }
+   }
+
 	pOut->ClearCommandsBar();
 	pOut->CreateDesignModeToolBar();  
 
 	pGrid->UpdateInterface();  
-	pGrid->SetEndGame(false);
+	pGrid->SetEndGame(true);
 }
 ToDesignModeAction::~ToDesignModeAction() {  //Created destructor
 
